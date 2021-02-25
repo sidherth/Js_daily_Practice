fb0b0298b89a0955e8e8630f49a103e33ee40f65
sidherth <sidherthsid@gmail.com>
2021-02-26 01:48:53 +0600

    json data update

--- javascript/script.js
+++ javascript/script.js
@@ -868,9 +868,28 @@ xmlhttp.onreadystatechange = function() {
 
     if (this.readyState == 4 && this.status == 200) {
         var data = this.responseText;
-        console.log(data);
+        //console.log(data);
+        jsonData(data);
     }
 
 };
 xmlhttp.open("GET", "data.json", true);
 xmlhttp.send();
+
+function jsonData(json_obj) {
+
+    // console.log(json_obj);
+    var js_obj;
+    js_obj = JSON.parse(json_obj);
+    // console.log(js_obj );
+    for (x in js_obj.persons) {
+        //console.log(x);
+
+        var persons = js_obj.persons;
+        //console.log(persons[x]);
+        for (y in persons[x]) {
+            console.log(persons[x][y]);
+        }
+    }
+
+}
